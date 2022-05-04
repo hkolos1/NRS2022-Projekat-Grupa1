acd1c407c422e4c77e983e8fe69852a58dcb4896
Emina <epalalic1@etf.unsa.ba>
2022-05-04 09:36:00 +0200

    Dodavanje proizvoda

--- app/src/main/java/com/example/roomapp/fragments/storageadmin/product/AddProductFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/storageadmin/product/AddProductFragment.kt
@@ -1,42 +1,45 @@
 package com.example.roomapp.fragments.storageadmin.product
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.example.roomapp.R
-import com.example.roomapp.fragments.admin.AdminFragmentArgs
+import com.example.roomapp.model.User
+import com.example.roomapp.viewmodel.LogViewModel
 import com.example.roomapp.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
-class ProductFragment : Fragment() {
+
+class AddProductFragment : Fragment() {
 
     private lateinit var mUserViewModel: UserViewModel
-    private lateinit var addButton : Button
-
+    private lateinit var mLogViewModel: LogViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_product, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_product, container, false)
 
         mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-        addButton = view.findViewById(R.id.addProdButton)
-        addButton.setOnClickListener {
-            addingProduct()
-        }
+
+
 
         return view
     }
 
-    private fun addingProduct() {
-        findNavController().navigate(R.id.action_productFragment_to_addProduct)
+    private fun insertDataToDatabase() {
+
+
     }
+
 
 }
