8613b3658c877b4e1d43e68dc6c0b6dd08046703
ezahirovic3 <ezahirovic3@etf.unsa.ba>
2022-05-14 12:19:22 +0200

    Revert "v1"
    This reverts commit cb0baa0c164e50fea8f3d88588a9c76ddcaa81cb.

--- app/src/main/java/com/example/roomapp/fragments/user/orders/UpdateOrderAdapter.kt
+++ app/src/main/java/com/example/roomapp/fragments/user/orders/UpdateOrderAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomapp.R
-import com.example.roomapp.model.Branch
 import com.example.roomapp.model.Product
 import kotlinx.android.synthetic.main.custom_row_order_product.view.*
 import kotlinx.android.synthetic.main.item_product.view.prName
@@ -13,8 +12,6 @@ import kotlinx.android.synthetic.main.item_product.view.prName
 class UpdateOrderAdapter: RecyclerView.Adapter<UpdateOrderAdapter.MyViewHolder>() {
 
     private var list = emptyList<Product>()
-    private var quantity = 0
-    private var branchProduct= emptyList<Product>()
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {}
 
@@ -30,24 +27,10 @@ class UpdateOrderAdapter: RecyclerView.Adapter<UpdateOrderAdapter.MyViewHolder>(
         val currentItem: Product = list[position]
         holder.itemView.prName.text = currentItem.prodName
         holder.itemView.kolicina.text = currentItem.quantity.toString()
-        quantity = holder.itemView.kolicina.text.toString().toInt()
-/*
-        holder.itemView.buttonplus.setOnClickListener{
-            if ((quantity+1)<=branchProduct[position].quantity) {
-                currentItem.quantity +=1
-            }
-        }
-
-        holder.itemView.buttonminus.setOnClickListener{
-            if ((quantity-1)>=0) {
-                currentItem.quantity -=1
-            }
-        }*/
     }
 
-    fun setData(order: List<Product>,branch: List<Product>){
+    fun setData(order: List<Product>){
         this.list = order
-        this.branchProduct = branch
         notifyDataSetChanged()
     }
 }
